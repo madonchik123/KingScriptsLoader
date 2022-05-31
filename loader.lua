d2d091b4eefb91328b9ab42f21f68d3c59c0c131
madonchik123 <68397448+madonchik123@users.noreply.github.com>
2022-05-31 21:03:17 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,21 @@
+if game.PlaceId == 9203864304 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/zReal-King/Raise-A-Floppa/main/Gui'))()
+elseif game.PlaceId == 6403373529 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/zReal-King/Slap-Battles/main/Gui'))()
+elseif game.PlaceId == 621129760 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/zReal-King/Knife-Ability-Test/main/Gui'))()
+elseif game.PlaceId == 3101667897 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/zReal-King/Legends-Of-Speed/main/Gui'))()
+elseif game.PlaceId == 2788229376 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/zReal-King/Da-Hood/main/Gui'))()
+elseif game.PlaceId == 2551991523 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/zReal-King/Broken-Bones-IV/main/Inf-Cash'))()
+elseif game.PlaceId == 70005410 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/zReal-King/Blox-Hunt/main/Gui'))()
+elseif game.PlaceId == 990566015 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/zReal-King/Cursed-Islands/main/Gui'))()
+elseif game.PlaceId == 3956818381 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/zReal-King/Ninja-Legends-2/main/Gui'))()
+elseif game.PlaceId == 205224386 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/zReal-King/Hide-and-Seek-Extreme/main/Gui'))()
+end
